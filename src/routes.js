6e0c17a41f76c358a49d43be6a5738f840482240
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-05 23:15:50 -0300

    install and configure react-navigation
    react-navigation changes a lot, so see the docs:
    reactnavigation.org/docs/en/getting-started.html
    
    yarn add react-navigation
    yarn add react-native-reanimated react-native-gesture-handler react-native-screens react-native-safe-area-context @react-native-community/masked-view
    yarn add react-navigation-stack
    
    react-native start --reset-cache

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import Main from './pages/Main';
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Main,
+    },
+    {
+      defaultNavigationOptions: {
+        headerTitleAlign: 'center',
+        headerBackTitleVisible: false,
+        headerStyle: {
+          backgroundColor: '#7159c1',
+        },
+        headerTintColor: '#FFF',
+      },
+    }
+  )
+);
+
+export default Routes;
