dc1daaf98c7fcd33fefaf9f0d10cd7152c7197f4
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-10 20:35:53 -0300

    add and configure Redux
    yarn add redux react-redux

--- src/index.js
+++ src/index.js
@@ -4,23 +4,28 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import 'react-native-gesture-handler';
+import { Provider } from 'react-redux';
 
 import { navigationRef } from './RootNavigation';
 import ActionBar from './components/ActionBar';
 import Main from './pages/Main';
 import Cart from './pages/Cart';
 
+import store from './store';
+
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer ref={navigationRef}>
-      <StatusBar barStyle="light-content" backgroundColor="#141419" />
-      <ActionBar />
-      <Stack.Navigator headerMode="none">
-        <Stack.Screen name="Home" component={Main} />
-        <Stack.Screen name="Cart" component={Cart} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer ref={navigationRef}>
+        <StatusBar barStyle="light-content" backgroundColor="#141419" />
+        <ActionBar />
+        <Stack.Navigator headerMode="none">
+          <Stack.Screen name="Home" component={Main} />
+          <Stack.Screen name="Cart" component={Cart} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
