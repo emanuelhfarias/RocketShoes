cf761e6c32dd0296dfb60ba2fc84e7e6f4c0147d
Emanuel H. Farias <emanuelfarias@outlook.com>
2020-03-09 21:25:28 -0300

    show Cart with fake items

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,6 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
+
 import { Text } from 'react-native';
 
-export default function Cart() {
-  return <Text>Cart</Text>;
+import {
+  Container,
+  Carrinho,
+  ProductList,
+  Product,
+  Total,
+  TotalText,
+  TotalPrice,
+  FinalizarButton,
+  TextFinalizarButton,
+  ProductDetails,
+  ProductPrice,
+  ProductImage,
+  ProductInfo,
+  ProductActions,
+  DeleteButton,
+  ProductInfoText,
+  DeleteButtonIcon,
+  SubTotalText,
+  TextInput,
+  CounterGroup,
+  ButtonIncrement,
+  ButtonDecrement,
+  IconIncrement,
+  IconDecrement,
+} from './styles';
+
+class Cart extends Component {
+  state = {
+    products: [
+      {
+        id: 1,
+        title: 'Tênis de Caminhada Leve Confortável',
+        price: 179.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg',
+        amount: 1,
+      },
+      {
+        id: 2,
+        title: 'Tênis VR Caminhada Confortável Detalhes Couro Masculino',
+        price: 139.9,
+        image:
+          'https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis2.jpg',
+        amount: 1,
+      },
+    ],
+  };
+
+  renderProduct = product => {
+    return (
+      <Product key={product.id}>
+        <ProductDetails>
+          <ProductImage source={{ uri: product.image }} />
+          <ProductInfo>
+            <ProductInfoText>{product.title}</ProductInfoText>
+            <ProductPrice>
+              <Text>R$ {product.price}</Text>
+            </ProductPrice>
+          </ProductInfo>
+          <DeleteButton>
+            <DeleteButtonIcon />
+          </DeleteButton>
+        </ProductDetails>
+        <ProductActions>
+          <CounterGroup>
+            <ButtonIncrement>
+              <IconIncrement />
+            </ButtonIncrement>
+            <TextInput value="1" />
+            <ButtonDecrement>
+              <IconDecrement />
+            </ButtonDecrement>
+          </CounterGroup>
+          <SubTotalText>R$ {product.price}</SubTotalText>
+        </ProductActions>
+      </Product>
+    );
+  };
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <Container>
+        <Carrinho>
+          <ProductList>
+            {products.map(product => this.renderProduct(product))}
+          </ProductList>
+          <Total>
+            <TotalText>TOTAL</TotalText>
+            <TotalPrice>R$ 179,90</TotalPrice>
+          </Total>
+          <FinalizarButton>
+            <TextFinalizarButton>Finalizar Pedido</TextFinalizarButton>
+          </FinalizarButton>
+        </Carrinho>
+      </Container>
+    );
+  }
 }
+
+export default Cart;
---
+++ src/pages/Cart/styles.js
@@ -0,0 +1,151 @@
+import styled from 'styled-components/native';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export const Container = styled.View`
+  height: 100%;
+  background: #141317;
+  padding-top: 20px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Carrinho = styled.ScrollView`
+  background: #fff;
+  width: 90%;
+  border-radius: 4px;
+`;
+
+export const ProductList = styled.View``;
+
+export const Product = styled.View`
+  padding: 15px;
+`;
+
+export const Total = styled.View``;
+
+export const TotalText = styled.Text`
+  font-size: 16px;
+  color: #999;
+  font-weight: bold;
+  text-align: center;
+`;
+
+export const TotalPrice = styled.Text`
+  font-size: 28px;
+  text-align: center;
+  font-weight: bold;
+`;
+
+export const FinalizarButton = styled.TouchableOpacity`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  align-self: center;
+
+  margin-top: 20px;
+  margin-bottom: 15px;
+  height: 50px;
+  width: 90%;
+
+  background: #7159c1;
+  border: 0;
+  border-radius: 4px;
+`;
+
+export const TextFinalizarButton = styled.Text`
+  font-size: 16px;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+`;
+
+export const ProductDetails = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ProductPrice = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+`;
+
+export const ProductInfoText = styled.Text`
+  font-size: 14px;
+  color: #444;
+`;
+
+export const ProductImage = styled.Image`
+  width: 90px;
+  height: 90px;
+`;
+
+export const ProductInfo = styled.View`
+  width: 150px;
+`;
+
+export const ProductActions = styled.View`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+  background: #ececec;
+  border-radius: 3px;
+  height: 40px;
+`;
+
+export const DeleteButton = styled.TouchableOpacity``;
+
+export const DeleteButtonIcon = styled(Icon).attrs({
+  name: 'delete-forever',
+  size: 32,
+  color: '#7159c1',
+})``;
+
+export const SubTotalText = styled.Text`
+  font-size: 18px;
+  font-weight: bold;
+  margin-right: 8px;
+`;
+
+export const TextInput = styled.TextInput`
+  background: #fff;
+  text-align: center;
+  height: 30px;
+  width: 60px;
+  padding: 3px;
+  border-radius: 4px;
+  border-width: 1px;
+  border-color: #ddd;
+`;
+
+export const CounterGroup = styled.View`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 100px;
+`;
+
+export const ButtonIncrement = styled.TouchableOpacity``;
+
+export const ButtonDecrement = styled.TouchableOpacity``;
+
+export const IconIncrement = styled(Icon).attrs({
+  name: 'add-circle-outline',
+  size: 18,
+  color: '#7159c1',
+})`
+  padding: 5px;
+`;
+
+export const IconDecrement = styled(Icon).attrs({
+  name: 'remove-circle-outline',
+  size: 18,
+  color: '#7159c1',
+})`
+  padding: 5px;
+`;
